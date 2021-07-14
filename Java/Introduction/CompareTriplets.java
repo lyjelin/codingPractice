24b744eaa33731cb6248edeaad164237b60b88ef
Yoon Jeong Lee <47136734+lyjelin@users.noreply.github.com>
2021-07-15 02:20:30 +0800

    Create CompareTriplets.java

---
+++ Java/Introduction/CompareTriplets.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> result = new ArrayList<>();
+        int ap=0; int bp=0;
+        
+        for (int i=0;i<a.size();i++){
+            if (a.get(i)>b.get(i)) ap++;
+            else if (a.get(i)<b.get(i)) bp++;
+        }
+        
+        result.add(ap);
+        result.add(bp);
+        
+        return result;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a.add(aItem);
+        }
+
+        String[] bItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
