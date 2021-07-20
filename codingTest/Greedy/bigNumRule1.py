d597903d864262d4806b1439b98ebff1ead306a5
lyjelin <u3560393@connect.hku.hk>
2021-07-20 11:24:59 +0800

    Modify # comment styling error

--- codingTest/Greedy/bigNumRule1.py
+++ codingTest/Greedy/bigNumRule1.py
@@ -1,6 +1,6 @@
-% python3 bigNumRule1
-% Enter 3 input in first line, i.e. 5 8 3
-% Enter list of integers in second line, i.e. 2 4 5 4 6
+# python3 bigNumRule1
+# Enter 3 input in first line, i.e. 5 8 3
+# Enter list of integers in second line, i.e. 2 4 5 4 6
 
 # Input
 N, M, K = map(int, input().split())
@@ -27,7 +27,7 @@ while True:
 
 print(result)
 
-''' 
+"""
 연속으로 더할 수 있는 휫수는 최대 K번이므로 
 '가장 큰 수를 K번 더하고 주번째로 큰 수를 한번 더하는 연산'을 반복하면 된다
-'''
+"""
