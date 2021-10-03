4327e014bebd78aa76153285472c206282e35a54
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2021-10-04 00:15:04 +0900

    Java Mark and Toy

---
+++ Java/JavaMarkAndToy.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'maximumToys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY prices
+     *  2. INTEGER k
+     */
+
+    public static int maximumToys(List<Integer> prices, int k) {
+    // Write your code here
+    
+        Collections.sort(prices);
+        
+        int len = prices.size();
+        int mid = len/2;
+        int end = len;
+        
+        int num = 0;
+        
+        if (prices.get(mid) >= k){
+            end = mid;
+        }
+        int count = 0;
+        for (int i = 0;i<end;i++){
+            num += prices.get(i);
+            if (num>k){
+                num = num - prices.get(i);
+                count = i;
+                break;
+            }
+        }
+        
+        return count;
+    
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
