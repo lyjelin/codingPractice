a4174221fbf1cd9e3c9e8a692024d826dbb0242c
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2021-10-09 22:14:07 +0900

    Debug Java Minimum Swap 2

--- Java/JavaMinimumSwap.java
+++ Java/JavaMinimumSwap.java
@@ -1,22 +1,22 @@
 public class JavaMinimumSwap {
     static int minimumSwaps(int[] arr) {
-        
-        int temp;
         int count = 0;
+        int temp = 0;
         
         for (int i=0;i<arr.length-1;i++){
             if (arr[i] != i+1){
                 for (int j=i+1;j<arr.length;j++){
-                    if (i+1 == arr[j]){
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                        count++;
+                    if (arr[j] == i+1) {
+                        temp = j;
+                        break;
                     }
-                    
                 }
+                arr[temp] = arr[i];
+                arr[i] = i+1;
+                count++;
+                
             }
         }
         return count;
-    } 
+    }
 }
