e38efd84a3a4cf1045523f18c3db5fb03aac1327
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2021-10-17 00:36:56 +0900

    Debug Java SubstringComparison

--- Java/JavaSubstringComparison.java
+++ Java/JavaSubstringComparison.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class JavaSubstringComparison {
 
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = s.substring(0, k);
