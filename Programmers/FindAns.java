27cd7fc99ef062cc4decbfad2d92b191d7c880cd
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2021-10-20 23:36:45 +0900

    Java find Ans

---
+++ Programmers/FindAns.java
@@ -0,0 +1,28 @@
+public class FindAns {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] nums = new String[numbers.length];
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {1, 2, 3, 4, 5};
+        int[] p3 = {1, 2, 3, 4, 5};
+        for (int i=0;i<numbers.length;i++){
+            nums[i]= String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(nums, new Comparator<String>(){
+            public int compare(String n1, String n2){
+                return (n2+n1).compareTo(n1+n2);
+            }
+        });
+        
+        if (nums[0].equals("0")) return "0";
+        
+        for (String s : nums){
+            answer+=s;
+        }
+        
+        
+        return answer;
+    }
+}
